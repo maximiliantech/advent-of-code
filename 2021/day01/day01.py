db782d6161be09abaf610329b10700b3cfbbcc7f
Maximilian Techritz <vemaximilian.t@gmail.com>
2022-07-20 00:41:48 +0200

    Solve part one

--- 2021/day01/day01.py
+++ 2021/day01/day01.py
@@ -1,1 +1,16 @@
 import sys
+
+# read input file and return the list of measurements
+def read_measurements(path):
+    with open(path) as file:
+        return [int(line.rstrip()) for line in file]
+
+# iterate over the list of measurements and find the number of times the measurement increases
+def find_number_of_times_measurement_increases(measurements):
+    number_of_times_measurement_increases = 0
+    for i in range(len(measurements) - 1):
+        if measurements[i] < measurements[i + 1]:
+            number_of_times_measurement_increases += 1
+    return number_of_times_measurement_increases
+
+measurements = read_measurements('input.txt')
