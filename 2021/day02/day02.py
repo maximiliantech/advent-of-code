e5592294fefcf1ce5a5238aa3687f7614f865c12
Maximilian Techritz <vemaximilian.t@gmail.com>
2022-08-01 10:34:54 +0200

    Solve day02 part 1

--- 2021/day02/day02.py
+++ 2021/day02/day02.py
@@ -5,3 +5,20 @@ import sys
 def read_measurements(path):
     with open(path) as file:
         return [line.rstrip() for line in file]
+
+def calculate_horizontal_and_depth(measurements):
+    horizontal_count = 0
+    depth_count = 0
+    for measurement in measurements:
+        instruction = measurement.split(' ')
+        match instruction[0]:
+            case 'forward':
+                horizontal_count += int(instruction[1])
+            case 'up':
+                depth_count -= int(instruction[1])
+            case 'down':
+                depth_count += int(instruction[1])        
+
+    return horizontal_count * depth_count
+
+print("Ergebnis von Tag 2.1:", calculate_horizontal_and_depth(read_measurements('./2021/day02/input.txt')))
