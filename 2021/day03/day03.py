f778fde043e1ce0906ca6adf94030436b72400ba
Maximilian Techritz <vemaximilian.t@gmail.com>
2022-08-05 20:06:21 +0200

    Add code comments

--- 2021/day03/day03.py
+++ 2021/day03/day03.py
@@ -1,10 +1,11 @@
 # Part 1
-# read input file and return the list of measurements
+''' read input file and return the list of measurements '''
 def read_diagnostic_report(path):
     with open(path) as file:
         # returns a 2D-array of the input binary numbers
         return [[int(bit) for bit in line.rstrip()] for line in file]
 
+''' Calculate the most and least common bits by counting the number of 1s and 0s in the array.'''
 def get_most_and_least_common_bits(array):
     count_zero = 0
     count_one = 0
@@ -17,6 +18,7 @@ def get_most_and_least_common_bits(array):
         else:
             count_zero += 1
     
+    # decide which bit is the most common and least common
     if count_zero > count_one:
         most_common_bit = 0
         least_common_bit = 1
@@ -28,15 +30,16 @@ def get_most_and_least_common_bits(array):
 
 
 def calculate_power_consumption(input):
-    # zip the input array for better iteration
+    # zip the original input array to iterate over the columns
     zipped_input = [list(bit) for bit in zip(*input)]
     gamma = ''
     epsilon = ''
     for array in zipped_input:
+        # find the most and least common bits
         most_common_bit, least_common_bit = get_most_and_least_common_bits(array)
         gamma += str(most_common_bit)
         epsilon += str(least_common_bit)
-
+    # mulitply the two binary numbers to get the integer power consumption
     return (int(gamma, 2) * int(epsilon, 2))
 
 report = read_diagnostic_report('./2021/day03/input.txt')
