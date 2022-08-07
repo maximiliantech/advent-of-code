4aa6b5020e409ead47412841f8138746514e5344
Maximilian Techritz <vemaximilian.t@gmail.com>
2022-08-07 20:27:28 +0200

    Add read sequence numbers from input file

--- 2021/day04/main.py
+++ 2021/day04/main.py
@@ -1,6 +1,22 @@
-def part_one(filename: str) -> int:
+from dataclasses import dataclass
+
+@dataclass
+class BingoCard:
+    pass
+
+def read_input(path: str) -> tuple[list[int], list[BingoCard]]:
+    with open(path) as f:
+        file = f.read().split("\n\n")
+
+    sequence = list(map(int, file[0].split(",")))
+
+    boards = []
+    return sequence, boards   
+
+def part_one(path: str) -> int:
+    sequence, borads = read_input(path)
     return 0
-    
+
 if __name__ == "__main__": 
-    input_path = "./day_04/input.txt"
+    input_path = "./2021/day04/input_test.txt"
     print("The result of Part 1:", part_one(input_path))
