be704ca0376e9f871d2fd7d1b58706b78b2e76da
Maximilian Techritz <vemaximilian.t@gmail.com>
2022-08-08 20:33:40 +0200

    Add basis of day 05 part 1

---
+++ 2021/day05/main.py
@@ -0,0 +1,6 @@
+def part_one(input_path) -> int:
+    return 0
+
+if __name__ == "__main__":
+    input_path = "./2021/day05/input_test.txt"
+    print("The result of Part 1:", part_one(input_path))
