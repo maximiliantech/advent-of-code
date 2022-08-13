3d283318f022b921ad108c7f53e4522d90ccf191
Maximilian Techritz <vemaximilian.t@gmail.com>
2022-08-13 12:14:28 +0200

    Add day02 part 2

--- 2021.playground/Pages/Day02.xcplaygroundpage/Contents.swift
+++ 2021.playground/Pages/Day02.xcplaygroundpage/Contents.swift
@@ -25,6 +25,33 @@ func partOne() -> String {
     return (x * y).description
 }
 
+func partTwo() -> String {
+    var x: Int = 0
+    var y: Int = 0
+    var aim: Int = 0
+    
+    let input = load(file: "input02", ofType: .txt)
+    
+    input?.split(separator: "\n").forEach { line in
+        let operation = line.split(separator: " ").first
+        let number = Int(line.split(separator: " ").last ?? "0")!
+        
+        switch operation {
+        case "down": aim += number
+        case "up": aim -= number
+        case "forward":
+            x += number
+            y += (aim * number)
+        default:
+            print("UNEXPECTED LINE \(line)")
+            fatalError()
+        }
+    }
+    
+    return (x * y).description
+}
+
 print(partOne())
+print(partTwo())
 
 //: [Next](@next)
