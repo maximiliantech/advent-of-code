9de33d78044312b84e3fa5a117e44e287890f528
Maximilian Techritz <vemaximilian.t@gmail.com>
2022-08-13 10:55:35 +0200

    Solve day01 part 1

--- 2021.playground/Pages/Day01.xcplaygroundpage/Contents.swift
+++ 2021.playground/Pages/Day01.xcplaygroundpage/Contents.swift
@@ -1,3 +1,19 @@
-import Cocoa
+import Foundation
 
-var greeting = "Hello, playground"
+let directoryURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first
+let fileURL = URL(fileURLWithPath: "input", relativeTo: directoryURL).appendingPathExtension("txt")
+
+func partOne() -> String {
+    let inputString = load(file: "input01")
+    if let numbers = inputString?.split(separator: "\n").map({ Int($0)! }) {
+        let result = zip(numbers, numbers.dropFirst())
+            .lazy
+            .filter{ $0 < $1 }
+            .count
+            .description
+        return result
+    }
+    return "No result!"
+}
+
+print(partOne())
---
+++ 2021.playground/Sources/Helpers.swift
@@ -0,0 +1,28 @@
+import Foundation
+
+public func readInput(from fileURL: URL) -> String {
+    do {
+     // Get the saved data
+     let savedData = try Data(contentsOf: fileURL)
+     // Convert the data back into a string
+     if let savedString = String(data: savedData, encoding: .utf8) {
+        return savedString
+     }
+    } catch {
+     // Catch any errors
+     print("Unable to read the file")
+    }
+    return ""
+}
+
+public func load(file named: String) -> String? {
+    guard let fileUrl = Bundle.main.url(forResource: named, withExtension: "txt") else {
+        return nil
+    }
+
+    guard let content = try? String(contentsOf: fileUrl, encoding: .utf8) else {
+        return nil
+    }
+    
+    return content
+}
