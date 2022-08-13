1c13230bda836c4cb5cad372efd9ed51e42d5536
Maximilian Techritz <vemaximilian.t@gmail.com>
2022-08-13 22:13:45 +0200

    Add load input day06

--- 2021.playground/Pages/Day06.xcplaygroundpage/Contents.swift
+++ 2021.playground/Pages/Day06.xcplaygroundpage/Contents.swift
@@ -2,6 +2,13 @@
 
 import Foundation
 
-var greeting = "Hello, playground"
+func partOne() -> String {
+    // load input and remove \n
+    let input = load(file: "input06", ofType: .txt)?.replacingOccurrences(of: "\n", with: "")
+    // turn input to Int Array
+    let numbers = input?.split(separator: ",").map({ Int($0)! })
+    return ""
+}
 
+print(partOne())
 //: [Next](@next)
