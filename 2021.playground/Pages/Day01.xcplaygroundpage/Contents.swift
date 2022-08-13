f203a8c1f503e8ba73a69ba3cda8a4832ee51c49
Maximilian Techritz <vemaximilian.t@gmail.com>
2022-08-13 10:57:03 +0200

    Del unused variables

--- 2021.playground/Pages/Day01.xcplaygroundpage/Contents.swift
+++ 2021.playground/Pages/Day01.xcplaygroundpage/Contents.swift
@@ -1,8 +1,5 @@
 import Foundation
 
-let directoryURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first
-let fileURL = URL(fileURLWithPath: "input", relativeTo: directoryURL).appendingPathExtension("txt")
-
 func partOne() -> String {
     let inputString = load(file: "input01")
     if let numbers = inputString?.split(separator: "\n").map({ Int($0)! }) {
