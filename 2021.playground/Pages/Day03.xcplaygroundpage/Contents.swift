30c4a9b0d2cb655b3846ccabde1b0e1e7d1eb7a5
Maximilian Techritz <vemaximilian.t@gmail.com>
2022-08-13 13:54:28 +0200

    Add func transform2DArray

--- 2021.playground/Pages/Day03.xcplaygroundpage/Contents.swift
+++ 2021.playground/Pages/Day03.xcplaygroundpage/Contents.swift
@@ -4,7 +4,22 @@ import Foundation
 
 func partOne() -> String {
     let input = load(file: "input03", ofType: .txt)
+    let bitList: [[Int]] = input?.split(separator: "\n").map { line in
+        line.map { $0.wholeNumberValue! }
+    } ?? [[0]]
+    
+    print(transform2DArray(array: bitList))
     return ""
+}
+
+func transform2DArray(array: [[Int]]) -> [[Int]] {
+    var resultArray: [[Int]] = []
+    for (j, line) in array.enumerated() {
+        for (i, bit) in line.enumerated() {
+            resultArray[i][j] = bit
+        }
+    }
+    return resultArray
 }
 
 print(partOne())
