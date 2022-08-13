a0d596df981ee2092f9f7ef3404d1ffd1a98ecf4
Maximilian Techritz <vemaximilian.t@gmail.com>
2022-08-13 12:11:21 +0200

    Add day 02 part 1

--- 2021.playground/Pages/Day02.xcplaygroundpage/Contents.swift
+++ 2021.playground/Pages/Day02.xcplaygroundpage/Contents.swift
@@ -3,7 +3,26 @@
 import Foundation
 
 func partOne() -> String {
-    return ""
+    var x: Int = 0
+    var y: Int = 0
+    
+    let input = load(file: "input02", ofType: .txt)
+    
+    input?.split(separator: "\n").forEach { line in
+        let operation = line.split(separator: " ").first
+        let number = Int(line.split(separator: " ").last ?? "0")!
+        
+        switch operation {
+        case "forward": x += number
+        case "down": y += number
+        case "up": y -= number
+        default:
+            print("UNEXPECTED LINE \(line)")
+            fatalError()
+        }
+    }
+    
+    return (x * y).description
 }
 
 print(partOne())
