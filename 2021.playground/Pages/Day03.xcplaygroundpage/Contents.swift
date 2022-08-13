91d44a2f883fa7110d04cdb81613c65309636ee2
Maximilian Techritz <vemaximilian.t@gmail.com>
2022-08-13 12:18:40 +0200

    Add structure and input of day03

---
+++ 2021.playground/Pages/Day03.xcplaygroundpage/Contents.swift
@@ -0,0 +1,12 @@
+//: [Previous](@previous)
+
+import Foundation
+
+func partOne() -> String {
+    let input = load(file: "input03", ofType: .txt)
+    return ""
+}
+
+print(partOne())
+
+//: [Next](@next)
