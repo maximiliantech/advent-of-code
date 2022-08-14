0a8d198b3137c99d7d488a7607d021b045972200
Maximilian Techritz <vemaximilian.t@gmail.com>
2022-08-14 14:59:31 +0200

    Solve day06 part 2

--- 2021.playground/Pages/Day06.xcplaygroundpage/Contents.swift
+++ 2021.playground/Pages/Day06.xcplaygroundpage/Contents.swift
@@ -4,7 +4,7 @@ import Foundation
 
 func partOne() -> String {
     // load input and remove \n
-    let input = load(file: "input06_test", ofType: .txt)?.replacingOccurrences(of: "\n", with: "")
+    let input = load(file: "input06", ofType: .txt)?.replacingOccurrences(of: "\n", with: "")
     // turn input to Int Array
     var numbers = input?.split(separator: ",").map({ Int($0)! })
     print("Initial state: ", numbers!)
@@ -23,7 +23,23 @@ func partOne() -> String {
 }
 
 func partTwo() -> String {
-    return ""
+    // load input and remove \n
+    let input = load(file: "input06", ofType: .txt)?.replacingOccurrences(of: "\n", with: "")
+    // turn input to Int Array
+    var numbers = input?.split(separator: ",").map({ Int($0)! })
+    print("Initial state: ", numbers!)
+    
+    for day in 1...257 {
+        for (index, lanternfish) in numbers!.enumerated() {
+            numbers![index] -= 1
+            if lanternfish == 0 {
+                numbers![index] = 6
+                numbers?.append(8)
+            }
+        }
+    }
+        
+    return numbers?.count.description ?? "0"
 }
 
 print(partOne())
