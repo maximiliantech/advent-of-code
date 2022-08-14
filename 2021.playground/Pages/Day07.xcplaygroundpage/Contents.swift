7e8f5b73c05a91d0d6f2c9aef6d2761c2cd3fa35
Maximilian Techritz <vemaximilian.t@gmail.com>
2022-08-14 16:08:11 +0200

    Solve day07 part 1

--- 2021.playground/Pages/Day07.xcplaygroundpage/Contents.swift
+++ 2021.playground/Pages/Day07.xcplaygroundpage/Contents.swift
@@ -2,8 +2,35 @@
 
 import Foundation
 
+struct CrabAlignment {
+    var totalFuel: Int
+    var position: Int
+    var horizontalPositions: [Int]
+    
+    init(for position: Int, horizontalPositions: [Int]) {
+        self.position = position
+        self.horizontalPositions = horizontalPositions
+        var total = 0
+        for curr_pos in horizontalPositions {
+            total += abs(curr_pos - position)
+        }
+        self.totalFuel = total
+    }
+}
+
 func partOne() -> String {
-    return ""
+    // load input and remove \n
+    let input = load(file: "input07_test", ofType: .txt)?.replacingOccurrences(of: "\n", with: "")
+    // turn numbers in [Int]
+    let horizontalPositions = input?.split(separator: ",").map { Int($0)! }
+    
+    var crabAlignmentArray: [CrabAlignment] = []
+    for align_pos in horizontalPositions! {
+        crabAlignmentArray.append(CrabAlignment(for: align_pos, horizontalPositions: horizontalPositions!))
+    }
+    // sort 
+    crabAlignmentArray.sort { $0.totalFuel < $1.totalFuel }
+    return crabAlignmentArray[0].totalFuel.description
 }
 
 print(partOne())
