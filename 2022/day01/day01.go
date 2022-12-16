78bcc70a1025de77f97280f91ef067bc44f4cc1e
Maximilian Techritz <vemaximilian.t@gmail.com>
2022-12-16 18:05:26 +0100

    Solve day01 part 01

--- 2022/day01/day01.go
+++ 2022/day01/day01.go
@@ -4,19 +4,46 @@ import (
 	"bufio"
 	"log"
 	"os"
+	"strconv"
 )
 
 func main() {
-	lines := readAllLines("./2022/day01/input_test.txt")
+	lines := readAllLines("./2022/day01/input.txt")
 
 	log.Println("Day 01 Part 01")
 	partOne(lines)
-
-	log.Println()
 }
 
 func partOne(lines []string) {
-	log.Println(lines)
+	elfCaloriesMap := getAllocatedCaloriesPerElf(lines)
+	elf, calories := getElfMostCalories(elfCaloriesMap)
+	log.Println(elf, calories)
+}
+
+func getElfMostCalories(mapp map[string]int) (string, int) {
+	var elf string = "1"
+	var calories int = 0
+	for key, value := range mapp {
+		if value > calories {
+			calories = value
+			elf = key
+		}
+	}
+	return elf, calories
+}
+
+func getAllocatedCaloriesPerElf(lines []string) map[string]int {
+	var elfCalories = make(map[string]int)
+	var elfNumber int = 1
+	for _, stringCalories := range lines {
+		if stringCalories == "" {
+			elfNumber++
+		} else {
+			calorie, _ := strconv.Atoi(stringCalories)
+			elfCalories[strconv.Itoa(elfNumber)] += calorie
+		}
+	}
+	return elfCalories
 }
 
 func readAllLines(filePath string) []string {
