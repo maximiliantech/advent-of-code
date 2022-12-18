00a3c42f5202bc48236a0d605233f1db6d8273be
Maximilian Techritz <vemaximilian.t@gmail.com>
2022-12-18 15:09:40 +0100

    First implementation of day01 part 01 in Swift

--- 2022.playground/Pages/Day01.xcplaygroundpage/Contents.swift
+++ 2022.playground/Pages/Day01.xcplaygroundpage/Contents.swift
@@ -2,6 +2,34 @@
 
 import Foundation
 
+struct Elf {
+    var name: String
+    var calories: [Int]
+    var caloriesSum: Int {
+        calories.reduce(0, +)
+    }
+    
+    init(name: String, calories: [Int]) {
+        self.name = name
+        self.calories = calories
+    }
+}
 
+func partOne() -> Int {
+    let input = load(file: "input01_test", ofType: .txt)!
+    var elves = getElves(from: input)
+    return 0
+}
+
+func getElves(from input: String) -> [Elf] {
+    var elvesCalories = input.split(separator: "\n\n")
+    var elves: [Elf] = []
+    for (index, cal) in elvesCalories.enumerated() {
+        print(cal)
+    }
+    return elves
+}
+
+print(partOne())
 
 //: [Next](@next)
