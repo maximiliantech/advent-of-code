a21c684cf31f7837d933344495dd71d190e4bdd2
Maximilian Techritz <vemaximilian.t@gmail.com>
2022-12-19 21:26:33 +0100

    Solve day02 part02

--- 2022-go/day02/day02.go
+++ 2022-go/day02/day02.go
@@ -11,6 +11,8 @@ func main() {
 	lines := readAllLines("./2022-go/day02/input.txt")
 	log.Println("Day 02 Part 01")
 	partOne(lines)
+	log.Println("Day 02 Part 02")
+	partTwo(lines)
 }
 
 func partOne(inputLines []string) {
@@ -24,6 +26,20 @@ func partOne(inputLines []string) {
 	}
 
 	solution := playRPC(opponent, me)
+	log.Println(solution)
+}
+
+func partTwo(inputLines []string) {
+	var opponent []string
+	var me []string
+
+	for _, line := range inputLines {
+		lineArray := strings.Split(line, " ")
+		opponent = append(opponent, lineArray[0])
+		me = append(me, lineArray[1])
+	}
+
+	solution := playRPCWithTarget(opponent, me)
 	log.Println(solution)
 }
 
@@ -78,6 +94,48 @@ func playRPC(opponent []string, me []string) int {
 	return score
 }
 
+func playRPCWithTarget(opponent []string, me []string) int {
+	rock := Rock{Score: 1, Opponent: "A", Me: "X"}
+	paper := Paper{Score: 2, Opponent: "B", Me: "Y"}
+	scissor := Scissor{Score: 3, Opponent: "C", Me: "Z"}
+	decision := Decision{Lose: "X", Draw: "Y", Win: "Z"}
+	score := Score{Lose: 0, Draw: 3, Win: 6}
+	totalScore := 0
+	for round := 0; round < len(opponent); round++ {
+		// --------------- Opponent chooses Rock ----------------
+		if (opponent[round] == rock.Opponent) && (me[round] == decision.Lose) {
+			totalScore += scissor.Score + score.Lose
+		}
+		if (opponent[round] == rock.Opponent) && (me[round] == decision.Draw) {
+			totalScore += rock.Score + score.Draw
+		}
+		if (opponent[round] == rock.Opponent) && (me[round] == decision.Win) {
+			totalScore += paper.Score + score.Win
+		}
+		// ---------------- Opponent chooses Scissor --------------------
+		if (opponent[round] == scissor.Opponent) && (me[round] == decision.Lose) {
+			totalScore += paper.Score + score.Lose
+		}
+		if (opponent[round] == scissor.Opponent) && (me[round] == decision.Draw) {
+			totalScore += scissor.Score + score.Draw
+		}
+		if (opponent[round] == scissor.Opponent) && (me[round] == decision.Win) {
+			totalScore += rock.Score + score.Win
+		}
+		// ------------ Opponent chooses Paper -----------------
+		if (opponent[round] == paper.Opponent) && (me[round] == decision.Lose) {
+			totalScore += rock.Score + score.Lose
+		}
+		if (opponent[round] == paper.Opponent) && (me[round] == decision.Draw) {
+			totalScore += paper.Score + score.Draw
+		}
+		if (opponent[round] == paper.Opponent) && (me[round] == decision.Win) {
+			totalScore += scissor.Score + score.Win
+		}
+	}
+	return totalScore
+}
+
 type Rock struct {
 	Score    int
 	Opponent string
@@ -94,6 +152,18 @@ type Scissor struct {
 	Score    int
 	Opponent string
 	Me       string
+}
+
+type Decision struct {
+	Lose string
+	Draw string
+	Win  string
+}
+
+type Score struct {
+	Lose int
+	Draw int
+	Win  int
 }
 
 func readAllLines(filePath string) []string {
