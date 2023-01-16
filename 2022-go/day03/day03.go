68d805ddf4fcb4a6d67631e6f8beab2ae7150e53
Maximilian Techritz <74709975+maximiliantech@users.noreply.github.com>
2023-01-16 17:12:00 +0100

    Add structure for day03

--- 2022-go/day03/day03.go
+++ 2022-go/day03/day03.go
@@ -1,5 +1,37 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"log"
+	"os"
+)
 
+func main() {
+	lines := readAllLines("./2022-go/day03/input_test.txt")
+	log.Println("Day 02 Part 01")
+	partOne(lines)
+}
+
+func partOne(inputLines []string) {
+
+}
+
+func readAllLines(filePath string) []string {
+	file, err := os.Open(filePath)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	lines := make([]string, 0)
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		lines = append(lines, line)
+	}
+
+	return lines
 }
