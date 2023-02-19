d18a7a9d000bc239e7bca9d627ab23ef1a52f811
Maximilian Techritz <74709975+maximiliantech@users.noreply.github.com>
2023-02-19 15:18:41 +0100

    Add day04 files

---
+++ 2022-go/day04/day04.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"bufio"
+	"log"
+	"os"
+)
+
+func main() {
+	lines := readAllLines("./input_test.txt")
+	log.Println("Day 04 Part 01")
+	partOne(lines)
+}
+
+func partOne(lines []string) {
+
+}
+
+func readAllLines(filePath string) []string {
+	file, err := os.Open(filePath)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	lines := make([]string, 0)
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		lines = append(lines, line)
+	}
+
+	return lines
+}
