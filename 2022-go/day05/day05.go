49e00202945fba34fd47127652bc086cce2b20e3
Maximilian Techritz <74709975+maximiliantech@users.noreply.github.com>
2023-02-21 07:06:33 +0100

    Add func for day05 part01

--- 2022-go/day05/day05.go
+++ 2022-go/day05/day05.go
@@ -1,17 +1,28 @@
 package main
 
 import (
+	"fmt"
 	"log"
 
 	"github.com/maximiliantech/advent-of-code/2022-go/common"
 )
 
 func main() {
-	lines := common.ReadAllLines("./input.txt")
+	lines := common.ReadAllLines("./input_test.txt")
 	log.Println("Day 05 Part 01")
 	partOne(lines)
 }
 
 func partOne(lines []string) {
 
+}
+
+func transformRerangeProdecure(lines []string) [][]int {
+	var procedure [][]int
+	for i, line := range lines {
+		p := make([]int, 3)
+		fmt.Sscanf(line, "move %d from %d to %d", &p[0], &p[1], &p[2])
+		procedure[i] = p
+	}
+	return procedure
 }
