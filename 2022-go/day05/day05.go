63a44cb04fc6f82305698b42d7577a41f6ca56af
Maximilian Techritz <74709975+maximiliantech@users.noreply.github.com>
2023-02-28 17:21:58 +0100

    Add day05 part02

--- 2022-go/day05/day05.go
+++ 2022-go/day05/day05.go
@@ -12,6 +12,8 @@ func main() {
 	lines := common.ReadAllLines("./input.txt")
 	log.Println("Day 05 Part 01")
 	partOne(lines)
+	log.Println("Day 05 Part 02")
+	partOne(lines)
 }
 
 func partOne(lines []string) {
@@ -45,7 +47,48 @@ func partOne(lines []string) {
 	log.Println(top_crates)
 }
 
+func partTwo(lines []string) {
+	max_height_stacks := 0
+	stacks_count := 0
+	procedure_string := []string{}
+	collect_procedure := false
+	procedure_count := 0
+
+	// check input for dimensions
+	for i, line := range lines {
+		if collect_procedure {
+			procedure_string = append(procedure_string, line)
+			procedure_count += 1
+		}
+
+		if len(line) == 0 {
+			collect_procedure = true
+			max_height_stacks = i - 1
+			l := lines[i-1]
+			stacks_count, _ = strconv.Atoi(string(l[len(l)-2]))
+		}
+	}
+
+	stacks := getCrates(lines, stacks_count, max_height_stacks)
+
+	procedure := transformRerangeProdecure(procedure_string, procedure_count)
+
+	stacks = rerangeWithAtOnce(stacks, procedure)
+	top_crates := getTopCrates(stacks)
+	log.Println(top_crates)
+}
+
 func rerange(stacks [][]byte, procedure [][]int) [][]byte {
+	for _, proc := range procedure {
+		for i := 0; i < proc[0]; i++ {
+			stacks[proc[2]-1] = append(stacks[proc[2]-1], stacks[proc[1]-1][len(stacks[proc[1]-1])-1])
+			stacks[proc[1]-1] = stacks[proc[1]-1][:len(stacks[proc[1]-1])-1]
+		}
+	}
+	return stacks
+}
+
+func rerangeWithAtOnce(stacks [][]byte, procedure [][]int) [][]byte {
 	for _, proc := range procedure {
 		for i := 0; i < proc[0]; i++ {
 			stacks[proc[2]-1] = append(stacks[proc[2]-1], stacks[proc[1]-1][len(stacks[proc[1]-1])-1])
