0b1a401725935d4c3a0add350dce0ccbc5153bca
Maximilian Techritz <74709975+maximiliantech@users.noreply.github.com>
2023-03-01 07:56:27 +0100

    Solve day06 part02

--- 2022-go/day06/day06.go
+++ 2022-go/day06/day06.go
@@ -10,6 +10,8 @@ func main() {
 	lines := common.ReadAllLines("./input.txt")
 	log.Println("Day 06 Part 01")
 	partOne(lines)
+	log.Println("Day 06 Part 02")
+	partTwo(lines)
 }
 
 func partOne(lines []string) {
@@ -17,6 +19,18 @@ func partOne(lines []string) {
 	var marker int = 0
 	for i, _ := range datastream {
 		if (i >= 3) && !duplicate(datastream[i-3:i+1]) {
+			marker = i + 1
+			break
+		}
+	}
+	log.Println(marker)
+}
+
+func partTwo(lines []string) {
+	var datastream string = lines[0]
+	var marker int = 0
+	for i, _ := range datastream {
+		if (i >= 13) && !duplicate(datastream[i-13:i+1]) {
 			marker = i + 1
 			break
 		}
