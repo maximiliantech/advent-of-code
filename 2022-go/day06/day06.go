e65380a55a9fcf9753f052e925747a7f8a0b7623
Maximilian Techritz <74709975+maximiliantech@users.noreply.github.com>
2023-03-01 07:52:50 +0100

    Solve day06 part01

--- 2022-go/day06/day06.go
+++ 2022-go/day06/day06.go
@@ -13,5 +13,24 @@ func main() {
 }
 
 func partOne(lines []string) {
+	var datastream string = lines[0]
+	var marker int = 0
+	for i, _ := range datastream {
+		if (i >= 3) && !duplicate(datastream[i-3:i+1]) {
+			marker = i + 1
+			break
+		}
+	}
+	log.Println(marker)
+}
 
+func duplicate(slice string) bool {
+	counts := map[rune]int{}
+	for _, char := range slice {
+		if counts[char] == 1 {
+			return true
+		}
+		counts[char]++
+	}
+	return false
 }
