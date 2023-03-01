e2ea58060f04fb667d8b584e2d3034dd2d5136be
Maximilian Techritz <74709975+maximiliantech@users.noreply.github.com>
2023-03-01 06:57:08 +0100

    Add files for day06 in go

---
+++ 2022-go/day06/day06.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"log"
+
+	"github.com/maximiliantech/advent-of-code/2022-go/common"
+)
+
+func main() {
+	lines := common.ReadAllLines("./input.txt")
+	log.Println("Day 06 Part 01")
+	partOne(lines)
+}
+
+func partOne(lines []string) {
+
+}
