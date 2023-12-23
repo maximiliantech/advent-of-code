f2a58ed476abdf2b433ad253d23bdf2e74095d0a
Maximilian Techritz <vemaximilian.t@gmail.com>
2023-12-23 23:52:49 +0100

    feat(day02): solve part02

--- 2023-go/day02/day02.go
+++ 2023-go/day02/day02.go
@@ -9,13 +9,14 @@ import (
 
 func main() {
 	lines := common.ReadAllLines("../2023-go/day02/input.txt")
+	games := readGames(lines)
 	log.Println("Day 02 Part 01")
-	partOne(lines)
+	partOne(games)
 	log.Println("Day 02 Part 02")
+	partTwo(games)
 }
 
-func partOne(lines []string) {
-	games := readGames(lines)
+func partOne(games []game) {
 	possibleGames := countPossibleGames(games)
 	log.Println(possibleGames)
 }
@@ -38,6 +39,33 @@ func isGamePossible(game game) bool {
 	}
 	return true
 }
+
+func partTwo(games []game) {
+	var sum int
+	for _, game := range games {
+		sub := findMinimumSubset(game)
+		sum += sub.red * sub.green * sub.blue
+	}
+	log.Println(sum)
+}
+
+func findMinimumSubset(game game) subset {
+	minimumSubset := subset{red: 0, green: 0, blue: 0} // start low to find the minimum
+	for _, subset := range game.subsets {
+		if subset.green > minimumSubset.green {
+			minimumSubset.green = subset.green
+		}
+		if subset.red > minimumSubset.red {
+			minimumSubset.red = subset.red
+		}
+		if subset.blue > minimumSubset.blue {
+			minimumSubset.blue = subset.blue
+		}
+	}
+	return minimumSubset
+}
+
+// ------- Helper functions to parse the input lines into structs of games and subsets -------
 
 type game struct {
 	id      int
